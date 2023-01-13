a1390f112b0d52499c3ccc3b83e91c6555c42f1a
TiagoAvelino <tiago.avelino1997@gmail.com>
2023-01-13 10:42:23 -0300

    Nova mensagem no home

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -8,15 +8,10 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
-    }
 }
 
 @Path("/")
-public class GreetingResource {
+public class Greeting {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
